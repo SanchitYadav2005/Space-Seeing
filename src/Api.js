46fb4d3615e66945cc5de37f18c20fabb660b27a
unknown <sanchit2005yudavanshi@gmail.com>
2023-07-13 09:56:23 +0530

    image is showing up

--- src/Api.js
+++ src/Api.js
@@ -2,15 +2,18 @@ import React, {Component} from "react";
 import axios from "axios";
 
 class Api extends Component{
-    
+    constructor(props){
+        super(props);
+        this.state = {url: ''}
+    }
     async componentDidMount(){
         const res = await axios.get("https://api.nasa.gov/planetary/apod?api_key=nGa6s8KYrPXDY86cgGAKlP4GeRoWOsm0GdbyMWgD")
         console.log(res.data)
-        
+        this.setState({url: res.data.url})
     }
     render(){
         return(
-            <img src={url}/>
+            <img src={this.state.url}/>
         )
     }
 }
