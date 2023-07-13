5ffc82fdc9a233b3745ddc982c7fcfa96ee918b0
unknown <sanchit2005yudavanshi@gmail.com>
2023-07-13 10:10:37 +0530

    got both the api on working mode

--- src/Api.js
+++ src/Api.js
@@ -7,9 +7,11 @@ class Api extends Component{
         this.state = {url: ''}
     }
     async componentDidMount(){
+        const res2 = await axios.get("http://images-api.nasa.gov/search?q=apollo+11")
         const res = await axios.get("https://api.nasa.gov/planetary/apod?api_key=nGa6s8KYrPXDY86cgGAKlP4GeRoWOsm0GdbyMWgD")
         console.log(res.data)
-        this.setState({url: res.data.url})
+        console.log(res2.data.collection.items[0].links[0].href)
+        this.setState({url: res2.data.collection.items[0].links[0].href})
     }
     render(){
         return(
