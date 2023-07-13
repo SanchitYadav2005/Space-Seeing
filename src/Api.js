3ff5d32e52cafa1cde902d75a136c67c27518800
unknown <sanchit2005yudavanshi@gmail.com>
2023-07-13 11:19:29 +0530

    nothing worked just displaying the imgae

--- src/Api.js
+++ src/Api.js
@@ -1,21 +1,25 @@
 import React, {Component} from "react";
 import axios from "axios";
 
+
+
+
 class Api extends Component{
     constructor(props){
         super(props);
-        this.state = {url: ''}
+        this.state = {url: '', alt: ''}
     }
     async componentDidMount(){
-        const res2 = await axios.get("http://images-api.nasa.gov/search?q=apollo+11")
-        const res = await axios.get("https://api.nasa.gov/planetary/apod?api_key=nGa6s8KYrPXDY86cgGAKlP4GeRoWOsm0GdbyMWgD")
-        console.log(res.data)
-        console.log(res2.data.collection.items[0].links[0].href)
-        this.setState({url: res2.data.collection.items[0].links[0].href})
+        const query = "apollo 11"
+        const res2 = await axios.get(`http://images-api.nasa.gov/search?q=${query}`)
+        this.setState({url: res2.data.collection.items[0].links[0].href, alt: res2.data.collection.items[0].links[0].render})
     }
     render(){
         return(
-            <img src={this.state.url}/>
+            <div>
+                <img src={this.state.url} alt={this.state.alt}/>
+            </div>
+            
         )
     }
 }
--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import Api from "./Api";
-import {
-  FluentProvider,
-  webLightTheme,
-  Button
-} from "@fluentui/react-components";
-import Form from "./Form";
+
 
 function App() {
   return (
-    <Form/>
+    <Api/>
   );
 }
 
